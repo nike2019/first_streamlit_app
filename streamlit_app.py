17db126e9a01ef26eee0d020882c10ea3d759620
nike2019 <46326222+nike2019@users.noreply.github.com>
2022-11-08 16:26:58 +0100

    Update streamlit_app.py
    Images reordered
    Header with Images added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
 
-streamlit.title('🥣 My Parents New Healthy Dinner')
-streamlit.header('🥗 Breakfast Menue')
-streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('🥣 Breakfast Menue')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
