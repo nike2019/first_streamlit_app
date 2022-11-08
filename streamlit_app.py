f3eaf6d24305c460102534d3c4bc7460207f5c44
nike2019 <46326222+nike2019@users.noreply.github.com>
2022-11-08 16:29:16 +0100

    Update streamlit_app.py
    Images reorganized

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('🥣 Breakfast Menue')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menue')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
