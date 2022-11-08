cf1f51c5b0363090ace9f2f18423835017422c22
nike2019 <46326222+nike2019@users.noreply.github.com>
2022-11-08 16:24:45 +0100

    Update streamlit_app.py
    Added some images to the titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menue')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('🥣 My Parents New Healthy Dinner')
+streamlit.header('🥗 Breakfast Menue')
+streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
