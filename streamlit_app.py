37610727b241253058d4cab6bece9ad88c2c8082
nike2019 <46326222+nike2019@users.noreply.github.com>
2022-11-08 16:40:41 +0100

    Update streamlit_app.py
    just show the selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_selected)
