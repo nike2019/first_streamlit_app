324e9761d08d3f4dd2cf9f6f8ad690fc22c36a76
nike2019 <46326222+nike2019@users.noreply.github.com>
2022-11-21 19:53:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT FROM FRUIT_LOAD_LIST")
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.haeder("The fuit load list contains:")
 streamlit.dataframe(my_data_rows)
